6ed35f7288d68c5d12c81accf8ce5aa3b3daab3b
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-14 13:58:10 +0000

    updated [ci skip]

--- _news/announcement_12.R
+++
@@ -1,7 +0,0 @@
----
-  layout: post
-date: 2024-5-15 09:30:00-0400
-inline: true
----
-  
-  New publication [Is intending to have children rightist? A research note on political ideology and fertility intentions](https://www.degruyter.com/document/doi/10.1515/spp-2023-0038/html) in Statistics, Politics and Policy.
